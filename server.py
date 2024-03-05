3b3e08323c49bfa268f1f1e24a12fa0a199793fc
DelaurierDev <delaurier4@gmail.com>
2024-03-04 18:07:07 -0700

    importing controllers into server.py

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask_app import app
-from flask_app.controllers import users
+from flask_app.controllers import users, transactions, notifications, friends, friend_requests,expenses,accounts
 
 
 if __name__ == '__main__':
