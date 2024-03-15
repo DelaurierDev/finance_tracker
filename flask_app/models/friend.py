c37b114ac9587597887b0782c1ddb34821f52d61
DelaurierDev <delaurier4@gmail.com>
2024-03-15 12:34:04 -0600

    setting up the friend class

---
+++ flask_app/models/friend.py
@@ -0,0 +1,11 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+mydb = 'n/a'
+
+class Friend:
+    def __init__(self, data):
+        relationship_id = data['relationship_id']
+        user_id = data['user_id']
+        friend_id = data['friend_id']
+        created_at = data['created_at']
+        updated_at = data['updated_at']
