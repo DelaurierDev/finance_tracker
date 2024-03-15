057ed549ea3e2ca7945db0ea2fb5d41b4bc47451
DelaurierDev <delaurier4@gmail.com>
2024-03-15 17:06:29 -0600

    setting up account and expense classes

---
+++ flask_app/models/account.py
@@ -0,0 +1,13 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+mydb = 'n/a'
+
+class Account:
+    def __init__(self, data):
+        account_id = data['account_id']
+        user_id = data['user_id']
+        account_name = data['account_name']
+        balance = data['balance']
+        isChecking = data['isChecking']
+        created_at = data['created_at']
+        updated_at = data['updatedd_at']
---
+++ flask_app/models/expense.py
@@ -0,0 +1,12 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask import flash
+mydb = 'n/a'
+
+class Expense:
+    def __init__(self, data):
+        expense_id = data['expense_id']
+        user_id = data['user_id']
+        amount = data['amount']
+        due_date = data['due_date']
+        created_at = data['created_at']
+        updated_at = data['updated_at']
