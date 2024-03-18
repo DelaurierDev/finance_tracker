c0298f41e19486be0e4b1cc903fbb4f5b85bb18e
DelaurierDev <delaurier4@gmail.com>
2024-03-18 12:21:01 -0600

    setup landing page

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,2 +1,13 @@
 from flask_app import app
 from flask import render_template, redirect,request,session,flash
+import re
+from flask_app.models.user import User
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+
+#Render the landing page
+@app.route('/')
+def login_reg():
+    return render_template('/index.html')
